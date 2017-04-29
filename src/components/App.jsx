592137cce153d9789a1f9073167639f39f2a788f
Ralph Maamari <Ralph Maamari>
2017-04-29 16:17:36 -0400

    Dispacthers Enabled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import '../App.css';
-
+import { connect } from 'react-redux';
+import { addReminder } from '../actions';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
+  addReminder() {
+    this.props.addReminder(this.state.text);
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,10 +27,12 @@ class App extends Component {
             <input
               className="form-control"
               placeholder="I have to..."
+              onChange={event => this.setState({text: event.target.value})}
             />
             <button
               type="button"
               className="btn btn-success"
+              onClick={() => this.addReminder}
             >
               Add Reminder
             </button>
@@ -29,4 +43,6 @@ class App extends Component {
   }
 }
 
-export default App;
+
+
+export default connect(null, { addReminder })(App);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducer from './reducers';
 
-const store = createStore();
+const store = createStore(reducer);
 
 ReactDOM.render(
   <Provider store={store}>
